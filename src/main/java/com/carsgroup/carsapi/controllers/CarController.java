df844fe643d9ad28fca011eb259000f54f888435
JefMartins <jeffersonmartins.jef@gmail.com>
2023-08-07 15:32:35 -0300

    adicionada rota de DELETE recebendo parâmetro pela url ex:/test/cars/{id}

--- src/main/java/com/carsgroup/carsapi/controllers/CarController.java
+++ src/main/java/com/carsgroup/carsapi/controllers/CarController.java
@@ -5,22 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carsgroup.carsapi.dto.CarDTO;
-import com.carsgroup.carsapi.dto.IdRequestDTO;
 import com.carsgroup.carsapi.models.Car;
 import com.carsgroup.carsapi.repository.CarRepository;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 @RestController
 @RequestMapping("/test/cars")
@@ -39,4 +35,8 @@ public class CarController {
     return repository.findAll();
   }
 
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id){
+    repository.deleteById(id);
+  }
 }
--- src/main/java/com/carsgroup/carsapi/dto/IdRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package com.carsgroup.carsapi.dto;
-
-public record IdRequestDTO (int id) {
-  
-}
